882769a5d5180094628fdc6016815997b91582cb
Tim0Tim <graden.bishop2995@omail.otech.edu>
2022-03-15 09:28:14 -0600

    Add project files.

---
+++ chap_6_practice2/AutoPolicy.cs
@@ -0,0 +1,38 @@
+﻿using System;
+namespace chap_6_practice2 {
+    class AutoPolicy
+    {
+        public int AccountNumber { get; set; }
+        public string MakeAndModel { get; set; }
+        public string State { get; set; }
+
+        public AutoPolicy(int accountNumber, string makeAndModel, string state)
+        {
+            AccountNumber = accountNumber;
+            MakeAndModel = makeAndModel;
+            State = state;
+        }
+
+
+
+        public bool IsNoFaultState
+        {
+            get
+            {
+                bool noFaultState;
+
+
+                switch (State)
+                {
+                    case "MA": case "NJ": case "NY": case "PA":
+                        noFaultState = true;
+                        break;
+                    default:
+                        noFaultState = false;
+                        break;
+                }
+                return noFaultState; 
+            }
+        }
+    }
+}
